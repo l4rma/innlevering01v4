72a137a9a5589fa259a002ffda0a20ecbb1f18da
Lars Magelssen <lars.magelssen@gmail.com>
2020-09-02 15:45:13 +0200

    Initial commit

---
+++ src/main/java/no/innlevering/QueryString.java
@@ -0,0 +1,4 @@
+package no.innlevering;
+
+public class QueryString {
+}
---
+++ src/test/java/no/innlevering/QueryStringTest.java
@@ -0,0 +1,12 @@
+package no.innlevering;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueryStringTest {
+    @Test
+    void arbitraryTest() {
+        assertEquals(1, 1);
+    }
+}
