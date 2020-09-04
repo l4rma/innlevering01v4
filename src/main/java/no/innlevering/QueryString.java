fba1493a507a4bbc1d1471e4306a2ecc30725262
Georg Tollefsen <georgto@me.com>
2020-09-05 00:42:55 +0200

    did somthing

--- src/main/java/no/innlevering/QueryString.java
+++ src/main/java/no/innlevering/QueryString.java
@@ -1,15 +1,24 @@
 package no.innlevering;
 
+import java.util.HashMap;
+
 public class QueryString {
 
 
     public static String getParameter(String parameterName, String testQuery) {
+        HashMap<String, String> convertedQuery = new HashMap<>();
+        int splitInt = testQuery.indexOf("&");
+        String firstPart = testQuery.substring(0,splitInt);
+        String secondPart = testQuery.substring(splitInt+1);
+        int equalsPos = firstPart.indexOf("=");
+        convertedQuery.put(firstPart.substring());
+
         String statusValue = "";
         if (parameterName == "status") {
             int indexOfEqual = testQuery.indexOf("=");
             statusValue = testQuery.substring(indexOfEqual + 1);
         }
-            return statusValue;
+            return convertedQuery.get(parameterName);
 
     }
 }
